1e7ce42a33d4f5436185217e775f8f2a0ac83e2a
Pedro-Gondim <pedro.neto@ufu.br>
2021-08-19 22:51:23 -0300

    P1

--- provas/p1/ex01/ex01.c
+++ provas/p1/ex01/ex01.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Fazer um programa para cadastro de livros. As informações para cadastro são: 
 // - o nome do livro
@@ -9,14 +10,36 @@
 
 
 // Definir as estruturas necessárias aqui:
-
+struct Livro{
+    char nome[40];
+    float preco;
+    int indicacao;
+    int ano;
+};
 
 int main(){
 
     // Ex 01: permitir que o usuário cadastre quantos livros quiser
     //  - ler do teclado a quantidade de livros que o usuário escolheu
     //  - ler do teclado as informações dos livros
+
+    int num;
+    char conf;
     printf("Informe quantos livros deseja cadastrar: ");
+    scanf("%d",&num);
+    struct Livro livros[num];
+    for(int i = 0; i< num;i++){
+        printf("digite o nome do livro: ");
+        scanf("%s",livros[i].nome);
+        printf("Digite o preco: ");
+        scanf("%f",&livros[i].preco);
+        printf("Digite 1 para um livro Novo ou 2 para Usado: ");
+        scanf("%d",&livros[i].indicacao);
+        printf("Digite o ano: ");
+        scanf("%d",&livros[i].ano);
+
+    }
+
   
     return 0;
 }
--- provas/p1/ex04/ex04.c
+++ provas/p1/ex04/ex04.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <math.h>
 
+struct Ponto
+{
+    double x;
+    double y;
+};
+
+
+struct Circulo
+{
+    struct Ponto ponto;
+    double raio;    
+};
+
+
 int main(){
 
    // Ex04: Um círculo pode ser definido com duas informações
@@ -19,6 +33,17 @@ int main(){
    // distancia d = sqtr(x*x + y*y)
    
    // Exemplo de saída: Circulo: Centro (3,5); Raio 2; Área: 12.56; Distancia: 5.0
+
+   struct Circulo val1;
+   val1.ponto.x = 3;
+   val1.ponto.y = 5;
+   val1.raio = 2;
+
+   float area = 3.14 * pow(val1.raio,2);
+   double distancia  = sqrt(val1.ponto.x*val1.ponto.x + val1.ponto.y*val1.ponto.y);
+   
+
+   printf("Circulo: Centro (%g,%g); Raio %g; Área: %g; Distancia: %g",val1.ponto.x,val1.ponto.y,val1.raio,area,distancia);
 
 
     return 0;
