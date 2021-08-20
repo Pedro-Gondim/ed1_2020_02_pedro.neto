a3bad6648cb1b07ae9551b11e5e5b44e4ab809c7
Pedro-Gondim <pedro.neto@ufu.br>
2021-08-19 21:43:12 -0300

    EX 3

--- provas/p1/ex03/ex03.c
+++ provas/p1/ex03/ex03.c
@@ -28,14 +28,73 @@
 
 */
 
+int lucro(float compra, float venda, float *porcento){
+    if (compra <= 0)
+        return -1;
+    *porcento = ((venda - compra) *  100)/compra;
+    if(*porcento <= 0){
+        *porcento = *porcento * -1;
+        return 1;
+    }
+    if(*porcento > 0 && *porcento <= 20){
+        return 2;
+    }
+    if(*porcento > 20 && *porcento <= 40){
+        return 3;
+    }
+    if(*porcento > 40){
+        return 4;
+    }
+
+    return 0;
+
+}
+
 int main(){
+    float compra;
+    float venda;
+    int return_lucro;
+    float* porcento = NULL;
+    float lucro_p;
+    porcento = &lucro_p;
 
    // Ex03: utilizando a função de cálculo de lucro, leia o preço de compra e venda
    // e mostre o percentual de lucro ou prejuízo e a classificação de acordo com
    // a tabela acima. 
    printf("Digite o preco de compra:");
-
+   scanf("%f",&compra);
    printf("Digite o preco de venda:");
+   scanf("%f",&venda);
+   return_lucro = lucro(compra,venda, porcento);
+
+   switch (return_lucro)
+   {
+    case -1:
+       printf("Valor invalido.");
+       return 0;
+       break;
+    case 1:
+       printf("Preço de compra: %g; Preco de venda:%g; Prejuízo de %g%%",compra,venda,*porcento);
+       return 0;
+       break;
+    case 2:
+       printf("Preço de compra: %g; Preco de venda:%g; Lucro pequeno de %g%%",compra,venda,*porcento);
+       return 0;
+       break;
+    case 3:
+       printf("Preço de compra: %g; Preco de venda:%g; Lucro bom de %g%%",compra,venda,*porcento);
+       return 0;
+       break;
+    case 4:
+       printf("Preço de compra: %g; Preco de venda:%g; Lucro alto de %g%%",compra,venda,*porcento);
+       return 0;
+       break;
+    
+   
+   default:
+        return -1;
+       break;
+   }
    
    // exemplo de saída:
    // Preço de compra: 10; Preco de venda:11; Lucro Pequeno de 10%
