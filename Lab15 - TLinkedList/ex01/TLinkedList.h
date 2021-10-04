c4d57ef0c8f5a9804ef425f3bfe36f7fc9543298
Pedro-Gondim <pedro.neto@ufu.br>
2021-10-03 23:54:40 -0300

    Lista encadeada

--- Lab15 - TLinkedList/ex01/TLinkedList.h
+++ Lab15 - TLinkedList/ex01/TLinkedList.h
@@ -12,12 +12,20 @@ struct aluno{
 
 typedef struct TLinkedList TLinkedList;
 
-TLinkedList *list_create();
-int list_free(TLinkedList *list);
-int list_push_front(TLinkedList *list, struct aluno al);
-int list_push_back(TLinkedList *list, struct aluno al);
-int list_insert_sorted(TLinkedList *list, struct aluno al);
-
-
-int list_print(TLinkedList *list);
+TLinkedList *list_create();                                         //ok
+int list_free(TLinkedList *list);                                   //ok
+int list_push_front(TLinkedList *list, struct aluno al);            //ok
+int list_push_back(TLinkedList *list, struct aluno al);             //ok
+int list_insert_sorted(TLinkedList *list, struct aluno al);         //ok
+int list_size(TLinkedList* list);                                   //ok
+int list_pop_front(TLinkedList* list);                              //ok
+int list_pop_back(TLinkedList* list);                               //ok
+int list_erase_data(TLinkedList* list, int mat);                    //ok
+int list_erase_pos(TLinkedList* list, int pos);                     //ok
+int list_find_pos(TLinkedList* list, int pos, struct aluno* al );   //ok  
+int list_find_mat(TLinkedList* list, int mat, struct aluno* al);    //ok
+int list_front(TLinkedList* list,struct aluno* al);                 //ok
+int list_back(TLinkedList* list, struct aluno* al);                 //ok
+int list_get_pos(TLinkedList* list, int mat, int* pos);             //ok
+int list_print(TLinkedList *list);                                  //ok
 
--- Lab15 - TLinkedList/ex01/prog.c
+++ Lab15 - TLinkedList/ex01/prog.c
@@ -1,9 +1,58 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "TLinkedList.h"
 
 int main(){
     TLinkedList *alunos_pet;
     alunos_pet = list_create();
+    TLinkedList *alunos_pet_front;
+    alunos_pet_front = list_create();
+    int matricula;
+    char nome[30];
+    float n1, n2, n3;
+
+    struct aluno aluno;
+    int ret;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Dados do alunos:");
+        scanf("%d%s%f%f%f", &matricula, nome, &n1, &n2, &n3);
+
+        aluno.matricula = matricula;
+        strcpy(aluno.nome, nome);
+        aluno.n1 = n1;
+        aluno.n2 = n2;
+        aluno.n3 = n3;
+
+        ret=list_push_back(alunos_pet, aluno);
+        ret =list_push_front(alunos_pet_front,aluno);
+        
+    }
+    list_print(alunos_pet);
+    list_print(alunos_pet_front);
+
+    list_pop_back(alunos_pet);
+    list_pop_front(alunos_pet_front);
+    
+    list_find_pos(alunos_pet,4,&aluno);
+    ret=list_push_back(alunos_pet, aluno);
+    ret =list_push_front(alunos_pet_front,aluno);
+    list_find_mat(alunos_pet,3,&aluno);
+    ret=list_push_back(alunos_pet, aluno);
+    ret =list_push_front(alunos_pet_front,aluno);
+
+    list_print(alunos_pet);
+    list_print(alunos_pet_front);
+
+    list_erase_pos(alunos_pet,4);
+    list_erase_data(alunos_pet_front,3);
+
+    list_print(alunos_pet);
+    list_print(alunos_pet_front);
+
+    list_free(alunos_pet);
+    list_free(alunos_pet_front);
     return 0;
 }
